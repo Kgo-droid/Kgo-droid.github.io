0dc65ad3ffc8eeb9aaf5c8142d75087c016c77ad
Kgo <bendania2004@gmail.com>
2025-12-27 06:30:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const btn = document.getElementById(`clickMeBtn`);
-const msg = document.getElementById(`message`);
+const username = "Kgo-droid";
+https://api.github.com/users/${username}/repos
 
-btn.addEventListener('click', () => {
-msg.textContent = "First website test";
-msg.style.color = "#351c75";
-}); 
+
+const listContainer = document.getElementById("project-list");
+async function getRepos() {
+const response = await fetch(url);
+const repositories = await response.json();
+repositories.forEach(repo => {
+
+const listItem = document.createElement('li');
+
+listItem.textContent = repo.name;
+listContainer.appendChild(listItem);
+});
+}
+getRepos();
